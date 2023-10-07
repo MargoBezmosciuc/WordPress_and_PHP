65ba0f99dcba16b14647a5e2329ed6400cf4be3e
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-10-07 22:40:59 +0200

    new Comment

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class("example"); ?>>
+    <?php wp_body_open(); ?>
+    <p>Halooooooooooooooooooo</p>
+
+<?php  ;?>
+
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
